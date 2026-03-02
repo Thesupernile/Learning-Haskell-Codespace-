bd3a9e8a24e00c7e2a3fb5a8a103cf20f1e19726
Thesupernile <62834678+Thesupernile@users.noreply.github.com>
2026-03-02 09:51:38 +0000

    Made a few mini functions

--- first.hs
+++ first.hs
@@ -1,1 +1,14 @@
-deleteMe = putStrLn "Hello! Add your code in this file. Feel free to delete this line!"
+double x = x + x
+doubleSum x y = double x + double y
+factorial x = product[1..x]
+mean xs = sum xs `div` length xs
+
+myLast xs = head (drop (length xs - 1) xs)
+myLast2 xs = xs !! max 0 (length xs - 1)
+
+second :: [a]->a
+second xs = xs !! 1
+
+
+palindrome :: Eq a => [a] -> Bool
+palindrome xs = reverse xs == xs
